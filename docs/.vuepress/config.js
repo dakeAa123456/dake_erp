4397d1242dc85b56af502508e1f404d693d0da23
wufeng <836843510@qq.com>
2024-12-20 17:09:50 +0800

    Initial commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,239 @@
+//const baiduCode = require('./config/baiduCode.js'); // 百度统计hm码
+//const htmlModules = require('./config/htmlModules.js');
+const nav = require('./navbar/nav.js'); // 在 .vuepress 目录下
+const { readFileList, readTotalFileWords, readEachFileWords } = require('./webSiteInfo/readFile');
+
+
+
+module.exports = {
+
+  theme: 'vdoing', // 使用依赖包主题
+  // theme: require.resolve('../../vdoing'), // 使用本地主题 (先将vdoing主题文件下载到本地：https://github.com/xugaoyi/vuepress-theme-vdoing)
+
+  //title: "vuepress-theme-study",
+  //description: '一个基于VuePress的 知识管理&博客 主题',
+  // base: '/', // 默认'/'。如果你想将你的网站部署到如 https://foo.github.io/bar/，那么 base 应该被设置成 "/bar/",（否则页面将失去样式等文件）
+  head: [ // 注入到页面<head> 中的标签，格式[tagName, { attrName: attrValue }, innerHTML?]
+    ['link', { rel: 'icon', href: 'https://vitepress.dev/vitepress-logo-large.webp' }], //favicons，资源放在public文件夹
+    ['meta', { name: 'keywords', content: 'vuepress,theme,blog,vdoing' }],
+    ['meta', { name: 'theme-color', content: '#11a8cd' }], // 移动浏览器主题颜色
+    ['link', { rel: 'stylesheet', href: '//at.alicdn.com/t/font_3114978_qe0b39no76.css' }],
+    ['meta', { name: 'referrer', content: 'no-referrer-when-downgrade' }],
+
+
+
+    //['meta', { name: 'wwads-cn-verify', content: '6c4b761a28b734fe93831e3fb400ce87' }], // 广告相关，你可以去掉
+    //['script', { src: 'https://cdn.wwads.cn/js/makemoney.js', type: 'text/javascript' }], // 广告相关，你可以去掉
+  ],
+
+  // 主题配置
+  themeConfig: {
+    nav:nav,
+    sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
+    logo: '/img/dake05.png', // 导航栏logo
+    //repo: 'xugaoyi/vuepress-theme-vdoing', // 导航栏右侧生成Github链接
+    searchMaxSuggestions: 10, // 搜索结果显示最大数
+    lastUpdated: '上次更新', // 更新的时间，及前缀文字   string | boolean (取值为git提交时间)
+    bodyBgImg:[  
+        '/img/dake011.jpg',
+        '/img/dake012.jpg',
+        '/img/dake013.jpg',
+        '/img/dake014.jpg',
+        '/img/dake015.jpg',
+        '/img/dake016.jpg',
+        '/img/dake017.jpg',
+        '/img/dake018.jpg',
+        '/img/dake019.jpg',
+
+      
+    ],  // 你的图片路径(必须位于 public 下)，可以是 URL
+    bodyBgImgOpacity: 0.9, // body 背景图透明度，选值 0 ~ 1.0, 默认0.5
+    bodyBgImgInterval:120,
+
+
+    // docsDir: 'docs', // 编辑的文件夹
+    // editLinks: true, // 编辑链接
+    // editLinkText: '编辑',
+
+    // 以下配置是Vdoing主题改动的和新增的配置
+    sidebar: { mode: 'structuring', collapsable: true }, // 侧边栏  'structuring' | { mode: 'structuring', collapsable: Boolean} | 'auto' | 自定义    温馨提示：目录页数据依赖于结构化的侧边栏数据，如果你不设置为'structuring',将无法使用目录页
+    // sidebarOpen: false, // 初始状态是否打开侧边栏，默认true
+    updateBar: { // 最近更新栏
+      showToArticle: false, // 显示到文章页底部，默认true
+      // moreArticle: '/archives' // “更多文章”跳转的页面，默认'/archives'
+    },
+    // titleBadge: false, // 文章标题前的图标是否显示，默认true
+    // titleBadgeIcons: [ // 文章标题前图标的地址，默认主题内置图标
+    //   '图标地址1',
+    //   '图标地址2'
+    // ],
+
+    pageStyle: 'card', // 页面风格，可选值：'card'卡片 | 'line' 线（未设置bodyBgImg时才生效）， 默认'card'。 说明：card时背景显示灰色衬托出卡片样式，line时背景显示纯色，并且部分模块带线条边框
+
+    // contentBgStyle: 1,
+
+    category: false, // 是否打开分类功能，默认true。 如打开，会做的事情有：1. 自动生成的frontmatter包含分类字段 2.页面中显示与分类相关的信息和模块 3.自动生成分类页面（在@pages文件夹）。如关闭，则反之。
+    tag: false, // 是否打开标签功能，默认true。 如打开，会做的事情有：1. 自动生成的frontmatter包含标签字段 2.页面中显示与标签相关的信息和模块 3.自动生成标签页面（在@pages文件夹）。如关闭，则反之。
+    // archive: false, // 是否打开归档功能，默认true。 如打开，会做的事情有：1.自动生成归档页面（在@pages文件夹）。如关闭，则反之。
+
+    author: { // 文章默认的作者信息，可在md文件中单独配置此信息 String | {name: String, href: String}
+      name: 'admin', // 必需
+      href: 'https://github.com/xugaoyi' // 可选的
+    },
+    social: { // 社交图标，显示于博主信息栏和页脚栏
+      // iconfontCssFile: '//at.alicdn.com/t/font_1678482_u4nrnp8xp6g.css', // 可选，阿里图标库在线css文件地址，对于主题没有的图标可自由添加
+      icons: [
+        {
+          iconClass: 'icon-youjian',
+          title: '发邮件',
+          link: 'https://qiye.aliyun.com/alimail/auth/login?custom_login_flag=1&reurl=%2Falimail%2F'
+        },
+        {
+          iconClass: 'icon-github',
+          title: 'GitHub',
+          link: 'https://github.com/xugaoyi'
+        },
+        {
+          iconClass: 'icon-erji',
+          title: '听音乐',
+          link: 'https://music.163.com/#/playlist?id=755597173'
+        }
+      ]
+    },
+    footer: { // 页脚信息
+      createYear: 2008, // 博客创建年份
+      copyrightInfo: '版权所有：阜阳大可新材料股份有限公司<a style="margin-left:15px;" href="https://beian.miit.gov.cn/?spm=5176.29458888.J_9220772140.60.7cf02868hQEYty#/Integrated/index">皖ICP备17001852号-1</a>', // 博客版权信息，支持a标签
+    },
+      // 站点配置（首页 & 文章页）
+  blogInfo: {
+    blogCreate: "2022-10-19", // 博客创建时间
+    indexView: true, // 开启首页的访问量和排名统计，默认 true（开启）
+    pageView: true, // 开启文章页的浏览量统计，默认 true（开启）
+    readingTime: true, // 开启文章页的预计阅读时间，条件：开启 eachFileWords，默认 true（开启）。可在 eachFileWords 的 readEachFileWords 的第二个和第三个参数自定义，默认 1 分钟 300 中文、160 英文
+    eachFileWords: readEachFileWords([""], 300, 160), // 开启每个文章页的字数。readEachFileWords(['xx']) 关闭 xx 目录（可多个，可不传参数）下的文章页字数和阅读时长，后面两个参数分别是 1 分钟里能阅读的中文字数和英文字数。无默认值。readEachFileWords() 方法默认排除了 article 为 false 的文章
+    mdFileCountType: "archives", // 开启文档数。1. archives 获取归档的文档数（默认）。2. 数组 readFileList(['xx']) 排除 xx 目录（可多个，可不传参数），获取其他目录的文档数。提示：readFileList() 获取 docs 下所有的 md 文档（除了 `.vuepress` 和 `@pages` 目录下的文档）
+    totalWords: "archives", // 开启本站文档总字数。1. archives 获取归档的文档数（使用 archives 条件：传入 eachFileWords，否则报错）。2. readTotalFileWords(['xx']) 排除 xx 目录（可多个，可不传参数），获取其他目录的文章字数。无默认值
+    moutedEvent: ".tags-wrapper", // 首页的站点模块挂载在某个元素后面（支持多种选择器），指的是挂载在哪个兄弟元素的后面，默认是热门标签 '.tags-wrapper' 下面，提示：'.categories-wrapper' 会挂载在文章分类下面。'.blogger-wrapper' 会挂载在博客头像模块下面
+    // 下面两个选项：第一次获取访问量失败后的迭代时间
+    indexIteration: 2500, // 如果首页获取访问量失败，则每隔多少时间后获取一次访问量，直到获取成功或获取 5 次后。默认 3 秒。注意：设置时间太低，可能导致访问量 + 2、+ 3 ......
+    pageIteration: 2500, // 如果文章页获取访问量失败，则每隔多少时间后获取一次访问量，直到获取成功或获取 5 次后。默认 3 秒。注意：设置时间太低，可能导致访问量 + 2、+ 3 ......
+    // 说明：成功获取一次访问量，访问量 + 1，所以第一次获取失败后，设置的每个隔段重新获取时间，将会影响访问量的次数。如 100 可能每次获取访问量 + 3
+  },
+    //htmlModules,
+  },
+
+  // 插件
+  plugins: [
+    
+     [require('./plugins/love-me'), { // 鼠标点击爱心特效
+       color: '#11a8cd', // 爱心颜色，默认随机色
+       excludeClassName: 'theme-vdoing-content' // 要排除元素的class, 默认空''
+     }],
+
+    ['fulltext-search'], // 全文搜索
+
+    // ['thirdparty-search', { // 可以添加第三方搜索链接的搜索框（原官方搜索框的参数仍可用）
+    //   thirdparty: [ // 可选，默认 []
+    //     {
+    //       title: '在GitHub中搜索',
+    //       frontUrl: 'https://github.com/search?q=', // 搜索链接的前面部分
+    //       behindUrl: '' // 搜索链接的后面部分，可选，默认 ''
+    //     },
+    //     {
+    //       title: '在npm中搜索',
+    //       frontUrl: 'https://www.npmjs.com/search?q=',
+    //     },
+    //     {
+    //       title: '在Bing中搜索',
+    //       frontUrl: 'https://cn.bing.com/search?q='
+    //     }
+    //   ]
+    // }],
+
+   // [
+    //  'vuepress-plugin-baidu-tongji', // 百度统计
+     // {
+     //   hm: baiduCode || '01293bffa6c3962016c08ba685c79d78'
+     // }
+    //],
+
+    ['one-click-copy', { // 代码块复制按钮
+      copySelector: ['div[class*="language-"] pre', 'div[class*="aside-code"] aside'], // String or Array
+      copyMessage: '复制成功', // default is 'Copy successfully and then paste it for use.'
+      duration: 1000, // prompt message display time.
+      showInMobile: false // whether to display on the mobile side, default: false.
+    }],
+    ['demo-block', { // demo演示模块 https://github.com/xiguaxigua/vuepress-plugin-demo-block
+      settings: {
+        // jsLib: ['http://xxx'], // 在线示例(jsfiddle, codepen)中的js依赖
+        // cssLib: ['http://xxx'], // 在线示例中的css依赖
+        // vue: 'https://jsd.cdn.zzko.cn/npm/vue/dist/vue.min.js', // 在线示例中的vue依赖
+        jsfiddle: false, // 是否显示 jsfiddle 链接
+        codepen: true, // 是否显示 codepen 链接
+        horizontal: false // 是否展示为横向样式
+      }
+    }],
+   // [
+    //  'vuepress-plugin-zooming', // 放大图片
+    //  {
+    //    selector: '.theme-vdoing-content img:not(.no-zoom)',
+    //    options: {
+    //      bgColor: 'rgba(0,0,0,0.6)'
+    //    },
+    //  },
+   // ],
+    [
+      '@vuepress/last-updated', // "上次更新"时间格式
+      {
+        transformer: (timestamp, lang) => {
+          const dayjs = require('dayjs') // https://day.js.org/
+          return dayjs(timestamp).format('YYYY/MM/DD, HH:mm:ss')
+        },
+      }
+    ],
+    [
+      'reading-progress'         //顶部进度条插件
+    ],
+    [
+      'ribbon',                     //彩带插件
+      {
+        size: 90, // width of the ribbon, default: 90
+        opacity: 0.8, // opacity of the ribbon, default: 0.3
+        zIndex: -1, // z-index property of the background, default: -1
+     }
+    ],
+    [
+      'dynamic-title',           // 网页标签地址栏变化插件
+      {
+         showIcon: '/favicon.ico',
+         showText: '(/≧▽≦/)咦！欢迎回来！',
+         hideIcon: '/failure.ico',
+         hideText: '(●—●)喔哟，不要走呀！',
+         recoverTime: 2000,
+      },
+   ],
+   [
+    {
+        name: 'custom-plugins',                   //全局展示时间插件
+        globalUIComponents: ["GlobalTip"] // 2.x 版本 globalUIComponents 改名为 clientAppRootComponentFiles
+    }
+    ],
+    [
+      {                                // 获取文章阅读量和字数
+        name: 'custom-plugins',
+        globalUIComponents: ["PageInfo"] // 2.x 版本 globalUIComponents 改名为 clientAppRootComponentFiles
+    },
+    ],                  
+  ],
+
+  markdown: {
+    // lineNumbers: true,
+    extractHeaders: ['h2', 'h3', 'h4', 'h5', 'h6'], // 提取标题到侧边栏的级别，默认['h2', 'h3']
+  },
+
+  // 监听文件变化并重新构建
+  extraWatchFiles: [
+    '.vuepress/config.js',
+    //'.vuepress/config/htmlModules.js',
+  ]
+}
---
+++ docs/.vuepress/navbar/nav.js
@@ -0,0 +1,104 @@
+// Java导航栏
+module.exports =  [
+  { text: '首页', link: '/' },
+  { text: '公司简介', link: '/pages/f93792/' },
+
+{
+  text: '产品分类',
+  items: [
+    {
+      text: '图文鉴赏',
+      items: [
+        { text: '电视柜', link: '/pages/f93792/' },
+        { text: '玄关柜', link: '/about/website/introduce/' },
+        { text: '餐边柜', link: '/about/website/introduce/'},
+        { text: '走廊柜', link: '/about/website/introduce/'},
+        { text: '衣帽柜', link: '/about/website/introduce/'},
+        { text: '阳台柜', link: '/about/website/introduce/'},
+        { text: '飘窗柜', link: '/about/website/introduce/'},
+        { text: '收纳柜', link: '/about/website/introduce/'},
+        { text: '床头柜', link: '/about/website/introduce/'},
+        { text: '酒柜', link: '/about/website/introduce/'},
+        { text: '鞋柜', link: '/about/website/introduce/'},
+      ]
+    },
+    {
+      text: '我的文档',
+      items: [
+        { text: '关于', link: '/about/website/introduce/' },
+      ]
+    },
+  ]
+},
+
+
+
+ { text: '资讯中心', 
+  items:[
+    { text: '旅游团建', link: '/pages/9cc27d/' },
+    { text: '工作会议', link: '/pages/1d82c3/' },
+    { text: '考察交流', link: '/pages/1d82c3/' },
+    { text: '产品质量', link: '/pages/1d82c3/' },
+    { text: '苔花家居', link: '/pages/3dda65/' },
+    { text: '校园招聘', link: '/pages/2dda55/' },
+  
+
+  ]
+  },
+
+
+ //{ text: '产品中心', link: '/pages/1b12ed/' },
+ { text: '招商加盟', link: '/pages/db78e2/' },
+ { text: '联系我们', link: '/pages/5d571c/' },
+ { text: '导航站', link: '/pages/daohang/' },
+
+
+
+  /*
+   {
+  text: '关于我们',
+items: [
+  { text: '轮子工具', link: '/whells-use/' },
+  { text: '项目工程', link: '/projects/' },
+  { text: '友情链接', link: '/friends/' }
+]
+},
+  {
+    text: '指南', link: '/pages/a2f161/', items: [
+      { text: '主题初衷与诞生', link: '/pages/52d5c3/' },
+      { text: '介绍', link: '/pages/a2f161/' },
+      { text: '快速上手', link: '/pages/793dcb/' },
+      { text: '目录结构', link: '/pages/2f674a/' },
+      { text: '核心配置和约定', link: '/pages/33d574/' },
+      { text: '自动生成front matter', link: '/pages/088c16/' },
+      { text: 'Markdown 容器', link: '/pages/d0d7eb/' },
+      { text: 'Markdown 中使用组件', link: '/pages/197691/' },
+      {
+        text: '相关文章', items: [
+          { text: '使目录栏支持h2~h6标题', link: '/pages/8dfab5/' },
+          { text: '如何让你的笔记更有表现力', link: '/pages/dd027d/' },
+          { text: '批量操作front matter工具', link: '/pages/2b8e22/' },
+          { text: '部署', link: '/pages/0fc1d2/' },
+          { text: '关于写文章和H1标题', link: '/pages/9ae0bd/' },
+          { text: '关于博客搭建与管理', link: '/pages/26997d/' },
+          { text: '在线编辑和新增文章的方法', link: '/pages/c5a54d/' },
+        ]
+      }
+    ]
+  },
+  
+  {
+    text: '配置', link: '/pages/a20ce8/', items: [
+      { text: '主题配置', link: '/pages/a20ce8/' },
+      { text: '首页配置', link: '/pages/f14bdb/' },
+      { text: 'front matter配置', link: '/pages/3216b0/' },
+      { text: '目录页配置', link: '/pages/54651a/' },
+      { text: '添加摘要', link: '/pages/1cc523/' },
+      { text: '修改主题颜色和样式', link: '/pages/f51918/' },
+      { text: '评论栏', link: '/pages/ce175c/' },
+    ],
+    
+  },
+
+  */
+]
