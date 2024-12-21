75a91597e036dc18f9b07aa9036ad348895ce823
wufeng <836843510@qq.com>
2024-12-21 13:03:10 +0800

    修改样式

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -12,7 +12,7 @@ module.exports = {
 
   //title: "vuepress-theme-study",
   //description: '一个基于VuePress的 知识管理&博客 主题',
-  base: '/', // 默认'/'。如果你想将你的网站部署到如 https://foo.github.io/bar/，那么 base 应该被设置成 "/bar/",（否则页面将失去样式等文件）
+  base: '/dake_erp/', // 默认'/'。如果你想将你的网站部署到如 https://foo.github.io/bar/，那么 base 应该被设置成 "/bar/",（否则页面将失去样式等文件）
   head: [ // 注入到页面<head> 中的标签，格式[tagName, { attrName: attrValue }, innerHTML?]
     ['link', { rel: 'icon', href: 'https://vitepress.dev/vitepress-logo-large.webp' }], //favicons，资源放在public文件夹
     ['meta', { name: 'keywords', content: 'vuepress,theme,blog,vdoing' }],
